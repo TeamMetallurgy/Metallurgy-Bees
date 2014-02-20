7e6ca646c5606c917eb81cc8ee2c69d455e01468
lclc98 <liam.c14785@gmail.com>
2014-02-20 16:39:33 +1000

    Fixed Buildscript

--- src/elcon/mods/metallurgybees/MBReference.java
+++ src/elcon/mods/metallurgybees/MBReference.java
@@ -4,7 +4,7 @@ public class MBReference {
 	
 	public static final String MOD_ID = "MetallurgyBees";
 	public static final String NAME = "Metallurgy Bees";
-	public static final String VERSION = "0.0.8";
+	public static final String VERSION = "0.1.0";
 	public static final String MC_VERSION = "[1.6.4]";
 	public static final String DEPENDENCIES = "required-after:Forge@[9.11.1.916,);required-after:Metallurgy3Base";
 	public static final String SERVER_PROXY_CLASS = "elcon.mods.metallurgybees.MBCommonProxy";
